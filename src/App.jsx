076372a5e4ac34507682d8e166cc111b1bc9e9d0
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-22 16:17:28 +0200

    fecth work print actor and actress in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const actressUrl = ("https://lanciweb.github.io/demo/api/actresses/")
+  const url =("https://lanciweb.github.io/demo/api/actors/")
+  const [actorData, setActorData] = useState([])
+  const [actressData, setActressData] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setActorData(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetch(actressUrl)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setActorData(data)
+    })
+  }, [])
 
   return (
     <>
-      
+      <h1>Actor/Actress</h1>
+      {actorData.map(actor=>{
+        <div className="container">
+          <div className="card">
+            
+          </div>
+        </div>
+      })}
     </>
   )
 }
